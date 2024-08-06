e2363b89901c55d05fa42bcb0c9ff1b0d8659c09
Braxton Mullings <braxtonlmg@gmail.com>
2024-08-06 14:21:16 +0000

    j

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    Braxton: "Mull0044!!"
+    Guest: "guest",
   },
 };
 export default config;
